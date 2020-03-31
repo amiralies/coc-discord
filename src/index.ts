9e73e1235fbb0a85875dc40c62e6f78fbe7dd9a8
Amirali Esmaeili <esmailiamirali@gmail.com>
2020-03-31 20:13:34 +0430

    Add basic presence implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,46 @@
-function activate() {}
+import { workspace } from 'coc.nvim';
+import { Client } from 'discord-rpc';
+import * as O from 'fp-ts/lib/Option';
+import { pipe } from 'fp-ts/lib/pipeable';
+
+const clientId = '694558978776105000';
+
+const setActivity = (client: Client, startTimestamp: number) => {
+  const details = pipe(
+    O.fromNullable(workspace.uri),
+    O.filter((x) => x.startsWith('file:///')),
+    O.map((x) => x.substr(6)),
+    O.map((x) => x.split('/')),
+    O.filter((xs) => xs.length > 0),
+    O.map((xs) => xs.reverse()[0]),
+    O.map((x) => `Editing ${x}`),
+    O.toUndefined,
+  );
+
+  const state = pipe(
+    O.fromNullable(workspace.root),
+    O.map((x) => x.split('/')),
+    O.filter((xs) => xs.length > 0),
+    O.map((xs) => xs.reverse()[0]),
+    O.map((x) => `On ${x}`),
+    O.toUndefined,
+  );
+
+  client.setActivity({ state, details, startTimestamp, instance: false });
+};
+
+const activate = () => {
+  const discordRpcClient = new Client({ transport: 'ipc' });
+
+  discordRpcClient.connect(clientId);
+  discordRpcClient.login({ clientId });
+
+  const startTimestamp = Date.now();
+
+  discordRpcClient.on('ready', () => {
+    setActivity(discordRpcClient, startTimestamp);
+    setInterval(() => setActivity(discordRpcClient, startTimestamp), 10000);
+  });
+};
 
 export { activate };
