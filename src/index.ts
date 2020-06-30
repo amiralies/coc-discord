3291cc587068ae6bc2e99d8e4145b684a3c60432
Bubba Lichvar <BubbatheVTOG@users.noreply.github.com>
2020-06-30 17:52:16 -0400

    OutputChannel Logger (#6)
    * Logger
    +feat(logger): Added a hand-rolled logger. Modified existing code to use
    logger.
    
    * OutputChannel Logger
    ~feat(logger): Added additional logger output.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import { workspace } from 'coc.nvim';
 import { Client } from 'discord-rpc';
 import * as O from 'fp-ts/lib/Option';
 import { pipe } from 'fp-ts/lib/pipeable';
+import { Logger } from './utils/logger';
 
-const clientId = '694558978776105000';
+const clientId:string = '694558978776105000';
+const logger:Logger = new Logger('discord');
+const ELAPSE_UPDATE_DURATION:number = 10000;
 
 const setActivity = (client: Client, startTimestamp: number) => {
   const details = pipe(
@@ -30,18 +33,18 @@ const setActivity = (client: Client, startTimestamp: number) => {
 };
 
 const activate = () => {
+  logger.info('Starting coc-discord...');
   const discordRpcClient = new Client({ transport: 'ipc' });
 
   discordRpcClient.connect(clientId);
-  // eslint-disable-next-line no-console
-  discordRpcClient.login({ clientId }).catch(() => console.warn('Could not connect coc-discord client to Discord.'));
-  // TODO Add output channel
+  discordRpcClient.login({ clientId }).catch((e) => logger.error(e));
 
   const startTimestamp = Date.now();
 
   discordRpcClient.on('ready', () => {
     setActivity(discordRpcClient, startTimestamp);
-    setInterval(() => setActivity(discordRpcClient, startTimestamp), 10000);
+    setInterval(() => setActivity(discordRpcClient, startTimestamp), ELAPSE_UPDATE_DURATION);
+    logger.info(`Started coc-discord client. Updating activity every ${ELAPSE_UPDATE_DURATION / 1000}s.`);
   });
 };
 
